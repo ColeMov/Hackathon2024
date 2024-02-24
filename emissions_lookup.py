766524fc4c71cdc2176ce2be2a70c371df56112b
ColeMov <124220870+ColeMov@users.noreply.github.com>
2024-02-24 14:59:49 -0600

    Fixed a ton of shit

--- emissions_lookup.py
+++ emissions_lookup.py
@@ -1,14 +1,25 @@
+# outputs CO2 emissions in g/mi then converts to tons for some mileage
 import pandas as pd
 
-file = pd.read_excel('CarData1975-2022.xlsx')
+
+def to_total_tons(emissions_g, mileage):
+    emissions_tons = (emissions_g * mileage) / 907185
+    return emissions_tons
+
+
+file = pd.read_excel('vehicle_data.xlsx')
 
 make = input("Vehicle Manufacturer: ")
 model = input("Vehicle Type: ")
 year = int(input("Model Year: "))
 
-cars_year = file[file['Model Year'] == year]
-cars_make = cars_year[cars_year['Manufacturer'] == make]
-cars_model = cars_make[cars_make['Vehicle Type'] == model]
+vehicle_emissions = file[['co2', 'make', 'model', 'year']]
+vehicle_emissions_valid = vehicle_emissions[vehicle_emissions['co2'] != -1]
+cars_year = vehicle_emissions_valid[vehicle_emissions_valid['year'] == year]
+cars_make = cars_year[cars_year['make'] == make]
+cars_model = cars_make[cars_make['model'] == model]
 
-emissions = cars_model.iloc[0, 5]
-print(emissions)
+mileage = int(input("Mileage: "))
+
+emissions_per_mile = cars_model.iloc[0, 0]
+print(to_total_tons(emissions_per_mile, mileage))
